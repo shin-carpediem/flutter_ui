0a29a44170f0a2eb735d241341aef6fa7f6d28e7
shin-carpediem <buru.aoshin@gmail.com>
2021-10-31 12:06:50 +0900

    add modal

--- lib/screen/courses_page/course_modal.dart
+++ lib/screen/courses_page/course_modal.dart
@@ -12,7 +12,7 @@ void modalBottomSheet(context) {
           ListTile(
             leading: Icon(Icons.edit),
             title: Text('Edit'),
-            onTap: () {},
+            onTap: () => editCoursesSheet(context),
           ),
           ListTile(
             leading: Icon(Icons.delete),
@@ -20,6 +20,53 @@ void modalBottomSheet(context) {
             onTap: () {},
           ),
         ],
+      );
+    },
+  );
+}
+
+void editCoursesSheet(context) {
+  showModalBottomSheet<void>(
+    context: context,
+    builder: (BuildContext context) {
+      return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: "Logo",
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: "Title",
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: "Subtitle",
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Save"),
+            ),
+          ],
+        ),
       );
     },
   );
