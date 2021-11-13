df22bbe76808d6078d837f03190f08a461c55fff
shin-carpediem <buru.aoshin@gmail.com>
2021-11-13 18:51:59 +0900

    hide with  env file

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-      GMSServices.provideAPIKey("AIzaSyBtdhuDvXyDcwegjYGFGJfHHph-QdALUQs")
+      GMSServices.provideAPIKey("${Google_Map_API}")
       GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_ui/app_theme.dart';
 // import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_ui/screen/top_page.dart';
 
 void main() async {
+  await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(
